d78978da08d3df23b03885b819c6c0fe07e1abcf
Lara Lasic <laralasic@Laras-MacBook-Pro.local>
2018-09-15 14:12:06 -0400

    skincare

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ console.log(city)
 city = "washington dc"
 console.log(city)
 
+let speed = 50;
+if (speed<30) {
+console.log("no ticket")
+}
+
+else if(speed<=40) {
+    console.log ("50 dollar fine")
+}
+
+else{
+    console.log("100 dollar fine")
+}
+let pro = prompt ("Hi! What do you want to buy?");
+
+let con = confirm ("Here is what I want");
+console.log(con)
+
+
+
