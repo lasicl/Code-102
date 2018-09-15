d002f90f3d5572ff7e5668a1d0fe75de82e5fc5d
Lara Lasic <laralasic@Laras-MacBook-Pro.local>
2018-09-15 11:30:47 -0400

    adding my javascript file

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+let box = "bottle"; 
+console.log(box);
+box = "phone";
+console.log(box);
+box = "wire"; 
+console.log(box); 
+let city;
+city = "zagreb";
+console.log(city)
+city = "washington dc"
+console.log(city)
+
